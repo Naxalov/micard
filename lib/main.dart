b5888d7f9038734e9fb2d79e3c18508d2e7065a8
Naxalov <uzbzarif@gmail.com>
2020-01-13 11:31:22 +0900

    add phone and email

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,66 @@ class MyApp extends StatelessWidget {
                         fontSize: 40,
                         fontWeight: FontWeight.w400),
                   )),
+              Text(
+                'Flutter developer'.toUpperCase(),
+                style: GoogleFonts.sourceSansPro(
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+//              Phone number
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+821079398919',
+                      style: GoogleFonts.sourceSansPro(
+                        color: Colors.teal[900],
+//                        letterSpacing: 2.5,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'uzbzarif@gmail.com',
+                      style: GoogleFonts.sourceSansPro(
+                        color: Colors.teal[900],
+//                        letterSpacing: 2.5,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
